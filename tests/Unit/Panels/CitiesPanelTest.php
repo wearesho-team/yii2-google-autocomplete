b479aa3ed702de641355cde9de1a540c90e7e55b
Alexander Letnikow <reclamme@gmail.com>
2018-10-17 16:29:26 +0300

    Add test case for catching query exception

--- tests/Unit/Panels/CitiesPanelTest.php
+++ tests/Unit/Panels/CitiesPanelTest.php
@@ -2,6 +2,9 @@
 
 namespace Wearesho\GoogleAutocomplete\Yii\Tests\Unit\Panels;
 
+use GuzzleHttp;
+use yii\web;
+use Wearesho\GoogleAutocomplete\Enums\SearchStatus;
 use Wearesho\GoogleAutocomplete\Yii\Panels\CitiesPanel;
 use Wearesho\GoogleAutocomplete\Yii\Tests\PanelTestCase;
 
@@ -14,6 +17,26 @@ use Wearesho\GoogleAutocomplete\Yii\Tests\PanelTestCase;
 class CitiesPanelTest extends PanelTestCase
 {
     protected const TESTED_PANEL = CitiesPanel::class;
+
+    public function testQueryException(): void
+    {
+        $this->mock(new GuzzleHttp\Psr7\Response(200, [], json_encode([
+            'status' => (string)SearchStatus::OVER_QUERY_LIMIT(),
+        ])));
+
+        $this->setQueryAttributes([
+            static::INPUT => 'input',
+            static::TOKEN => $this->token,
+            static::LANGUAGE => static::RUSSIAN,
+        ]);
+
+
+        $this->expectException(web\HttpException::class);
+        $this->expectExceptionCode(2);
+        $this->expectExceptionMessage('Google autocomplete API query error: OVER_QUERY_LIMIT');
+
+        $this->instancePanel()->getResponse();
+    }
 
     public function testFullResponse(): void
     {
